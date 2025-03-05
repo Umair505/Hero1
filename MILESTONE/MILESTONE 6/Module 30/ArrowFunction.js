cd793e3bcc362f11491f086c8d2af761240efc5d
Umair505 <moinuli359@gmail.com>
2025-03-05 20:23:08 +0600

    Arrow Function

---
+++ MILESTONE/MILESTONE 6/Module 30/ArrowFunction.js
@@ -0,0 +1,13 @@
+// function add(a,b)
+// {
+//     return a + b;
+// }
+// add(1,2)
+// const sum2 = function(a,b){
+//     return a + b;
+// }
+// console.log(sum2(1,2));
+
+const add = (a,b) => a + b;
+
+console.log(add(1,2));
