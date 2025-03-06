1f51a9303af31423bd50a1fcf36a285bc0e8d3f7
Umair505 <moinuli359@gmail.com>
2025-03-06 18:55:33 +0600

    updated

--- MILESTONE/MILESTONE 6/Module 30/ArrowFunction.js
+++ MILESTONE/MILESTONE 6/Module 30/ArrowFunction.js
@@ -11,3 +11,13 @@
 const add = (a,b) => a + b;
 
 console.log(add(1,2));
+
+const sqr = a =>a*a;
+
+console.log(sqr(5));
+
+document.getElementById("title").addEventListener("click",
+    () =>{
+        alert("Title Clicked");
+    }
+)
