415e50063256eb208b8c8913396e0d369a763905
Umair505 <moinuli359@gmail.com>
2025-03-06 19:03:00 +0600

    spread.js

---
+++ MILESTONE/MILESTONE 6/Module 30/spread.js
@@ -0,0 +1,18 @@
+const friends = ["Sakib","Rakib","Akib"];
+// console.log(...friends);
+
+// const newArray = ["Hero Alom", ...friends];
+// console.log(...newArray);
+const numbers = [1,34,2,12,4,2,2,1,100];
+
+//Duplicate value delete will be deleted
+const uniqueNumbers = [...new Set(numbers)];
+console.log(...uniqueNumbers);
+
+//Find maximum value
+const max = Math.max(...uniqueNumbers);
+console.log(max);
+
+//Find minimum value
+const min = Math.min(...numbers);
+console.log(min);
