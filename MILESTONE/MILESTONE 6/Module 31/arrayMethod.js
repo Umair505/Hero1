b56a5c29a1bcd93454870a8a897ae1a199ac240c
Umair505 <moinuli359@gmail.com>
2025-03-07 20:17:55 +0600

    arrayMethod

---
+++ MILESTONE/MILESTONE 6/Module 31/arrayMethod.js
@@ -0,0 +1,24 @@
+const products = [
+    {id: 1,name : "Iphone",color:"black",price:"12000","brand":"apple"},
+    {id: 2,name : "Samsung",color:"white",price:"10000","brand":"samsung"},
+    {id: 3,name : "OnePlus",color:"red",price:"8000","brand":"oneplus"},
+    {id: 4,name : "Redmi",color:"blue",price:"7000","brand":"xiaomi"},
+    
+];
+//for each return nothing
+//Map return array
+// products.forEach((product,index) =>{
+//     if(product.color === "black" || product.color === "red"){
+//         console.log(product.name);
+//     }
+// })
+
+//Filter return new array
+// const newArr = products.filter(product =>product.price > 8000);
+// console.log(newArr);
+//Find : It find through loop when it finds it return imediate one 
+// const product = products.find(p => p.id === 3)
+// console.log(product);
+
+const newProducts =products.filter(p => p.id != 2);
+console.log(newProducts);
