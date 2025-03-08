65247db622cfc836d9acc924c1ac750a8ef5482f
Umair505 <moinuli359@gmail.com>
2025-03-08 18:58:39 +0600

    updated

--- MILESTONE/MILESTONE 6/Module 31/mapRecap.js
+++ MILESTONE/MILESTONE 6/Module 31/mapRecap.js
@@ -5,12 +5,22 @@ const products = [
     {id: 4,name : "Redmi",color:"blue",price:7000,brand:"xiaomi"},
     {id: 5,name : "Nokia",color:"green",price:6000,brand:"nokia"},
     {id: 6,name : "Sony",color:"yellow",price:9000,brand:"sony"},
+    {id: 7,name : "Iphone",color:"blue",price:12000,brand:"apple"},
 ];
-const newProducts = products.map(p => {
-    if( p.brand === "apple")
-    {
-     p.price = p.price + 100;
-    }
-    return p;
-})
-console.log(newProducts)
+// const newProducts = products.map(p => {
+//     if( p.brand === "apple")
+//     {
+//      p.price = p.price + 100;
+//     }
+//     return p;
+// })
+// console.log(newProducts)
+
+//for each
+// products.forEach(product => console.log(product));
+
+// const singleProduct = products.find(p => p.id === 3);
+// console.log(singleProduct);
+
+const newProduct2 = products.filter(p => p.name === "Iphone");
+console.log(newProduct2);
