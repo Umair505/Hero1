706ec69a88c315a05d99a9113cf1165d267a8cbf
Umair505 <moinuli359@gmail.com>
2025-03-09 23:21:01 +0600

    updated

--- MILESTONE/MILESTONE 6/Module 31/this.js
+++ MILESTONE/MILESTONE 6/Module 31/this.js
@@ -18,6 +18,13 @@ class person{
     }
 }
 const Abrar = new person("Abrar","24","Single");
-console.log(Abrar);
-Abrar.greet();
-console.log(Abrar.status);
+// console.log(Abrar);
+// Abrar.greet();
+// console.log(Abrar.status);
+
+// const [a, b] = [1,2,3,4,45,5]; 
+// console.log(a+b);
+
+const {x, y, z} = {x: 1, y1: 2, z: 3};
+
+console.log(x, y, z);
