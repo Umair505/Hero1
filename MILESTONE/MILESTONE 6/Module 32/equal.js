2ecebcb68d1b6937147e24d459b9fcf5ad478a80
Umair505 <moinuli359@gmail.com>
2025-03-10 05:45:36 +0600

    equal.js

---
+++ MILESTONE/MILESTONE 6/Module 32/equal.js
@@ -0,0 +1,23 @@
+// const n1 = 10;
+// const n2 = "10";
+const n1 = [];
+const n2 = [];
+// const n2 = n1;//Both numbers are equal
+//output Both numbers are not equal
+
+// if(n1==n2)//without data type check
+// if(n1===n2) //With data type check
+// {
+//     console.log("Both numbers are equal");
+// }
+// else
+//     console.log("Both numbers are not equal");
+
+    
+    if(5*"2"===10) //With data type check
+    {
+        console.log("Both numbers are equal");
+    }
+    else
+        console.log("Both numbers are not equal");
+    //output Both numbers are equal because type coercion
