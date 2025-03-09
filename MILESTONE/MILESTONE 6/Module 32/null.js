1d1ea1a97f822edf94ad28eb93caeb0cdf882dab
Umair505 <moinuli359@gmail.com>
2025-03-10 05:30:01 +0600

    null.js

---
+++ MILESTONE/MILESTONE 6/Module 32/null.js
@@ -0,0 +1,18 @@
+// const name = "Moinul";
+// console.log(typeof name);
+// let bname;
+
+// console.log(typeof bname);//undefined
+
+let name = null;
+console.log(name);//null    
+console.log(typeof name);//object this is a bug in js
+
+// let name = undefined;
+// function sum(num1 ,num2)
+// {
+//     console.log(num1,num2);
+// }
+// sum();//undefined
+// const result = sum(10,20);
+// console.log(result) //Undefined
