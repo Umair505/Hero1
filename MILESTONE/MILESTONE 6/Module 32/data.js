9e50303594d4514c70837acf8fc3737a63db2f4a
Umair505 <moinuli359@gmail.com>
2025-03-10 05:19:05 +0600

    data.js

---
+++ MILESTONE/MILESTONE 6/Module 32/data.js
@@ -0,0 +1,17 @@
+// const name = "gias";//primitive
+
+// const person = ["John",15];//Non primitive
+// let person = "Moinul";
+
+// let NewPerson = person;
+
+// person = "Umair";
+
+// console.log(person);//Umair
+// console.log(NewPerson);//Moinul
+
+let person = {name : "Rakib"};
+let NewPerson = person;//array reference ta rakhe location store kore
+person.name = "Sakib";
+
+console.log(person); //Sakib
