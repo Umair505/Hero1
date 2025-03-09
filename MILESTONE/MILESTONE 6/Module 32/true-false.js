fd9f32129709a3712e99573e4867adbc1ebd456e
Umair505 <moinuli359@gmail.com>
2025-03-10 05:36:32 +0600

    True-False.js

---
+++ MILESTONE/MILESTONE 6/Module 32/true-false.js
@@ -0,0 +1,14 @@
+// const name = "MOINUL ISLAM";
+
+// let name = "";//false
+// let name = -1;//True
+const name = [];//True
+if(name){
+    console.log('Hello World!');
+}
+else{
+    console.log('Goodbye World!');
+}
+
+const fname = "Moinul";
+console.log(!fname);//false
