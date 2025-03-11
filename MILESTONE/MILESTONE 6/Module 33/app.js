6bcc10c1f0986727c5ec292281b396dde39064c3
Umair505 <moinuli359@gmail.com>
2025-03-11 19:31:44 +0600

    app.js

---
+++ MILESTONE/MILESTONE 6/Module 33/app.js
@@ -0,0 +1,10 @@
+const person ={
+    name: "Hero Alom",
+    age: 25,
+    city: "Dhaka",
+    friends: ["Bapparaj", "Moinul"],
+    status: "Single"
+}
+const newPerson = JSON.stringify(person);
+
+console.log(newPerson);
