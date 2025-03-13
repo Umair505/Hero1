72917217c8f6a68aff1844d77524807eab9435f0
Umair505 <moinuli359@gmail.com>
2025-03-13 23:48:46 +0600

    updated

--- MILESTONE/MILESTONE 6/Module 34/eventLoop.js
+++ MILESTONE/MILESTONE 6/Module 34/eventLoop.js
@@ -14,4 +14,35 @@ function c(){
 function d(){
     console.log('I am in d()');
 }
-a();
+// a();
+
+async function myFunction() {
+    return "Hello";
+   }
+   myFunction().then(console.log);
+
+   let message = 'Hello!'; 
+setTimeout(() => {
+ message = 'Goodbye!'; 
+}, 0); 
+console.log(message);
+
+try {
+    console.log("Start");
+    throw new Error("Error occurred");
+   } catch (err) {
+    console.log(err.message);
+   } finally {
+    console.log("End");
+   }
+
+   console.log("A");
+   Promise.resolve().then(() => console.log("B"));
+   setTimeout(() => console.log("C"), 0);
+   console.log("D");
+
+   let x = 1;
+let intervalId = setInterval(() => {
+ console.log(x++);
+ if (x > 3) clearInterval(intervalId);
+}, 1000);
