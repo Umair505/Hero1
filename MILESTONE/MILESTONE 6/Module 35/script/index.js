a8f66a29ca6a0430b81623d3aa9a2b56bfe37882
Umair505 <moinuli359@gmail.com>
2025-03-15 01:00:22 +0600

    updated

--- MILESTONE/MILESTONE 6/Module 35/script/index.js
+++ MILESTONE/MILESTONE 6/Module 35/script/index.js
@@ -14,4 +14,38 @@ function displayCategories(categories){
         categoryContainer.append(categoryDiv);
     }
 }
+// {category_id: '1001', video_id: 'aaaa', thumbnail: 'https://i.ibb.co/L1b6xSq/shape.jpg', title: 'Shape of You', authors: Array(1), …}
+// Load Videos
+const loadVideos = () =>{
+    fetch(" https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data)=>displayVideos(data.videos));
+}
+const displayVideos = (videos) =>{
+    videos.forEach((video)=>{
+        console.log(video)
+        const videoContainer = document.getElementById("video-container");
+        const videoCard = document.createElement("div");
+        videoCard.innerHTML = `
+<div class="card bg-base-100 shadow-sm w-full h-full">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Video" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `
+        videoContainer.append(videoCard);
+    })
+}
+
+
+loadVideos();
 loadCategory();
