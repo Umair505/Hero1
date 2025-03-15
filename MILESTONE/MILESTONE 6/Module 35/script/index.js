3816d22f44b60bafdeb83c200877357e21a0a317
Umair505 <moinuli359@gmail.com>
2025-03-15 19:14:52 +0600

    toogle loader

--- MILESTONE/MILESTONE 6/Module 35/script/index.js
+++ MILESTONE/MILESTONE 6/Module 35/script/index.js
@@ -1,3 +1,12 @@
+const showLoader = () =>{
+    document.getElementById("loader").classList.remove("hidden");
+    document.getElementById("video-container").classList.add("hidden");
+}
+const hideLoader = () =>{
+    document.getElementById("loader").classList.add("hidden");
+    document.getElementById("video-container").classList.remove("hidden");
+}
+
 function removeActiveClass(){
     const activeButtons = document.getElementsByClassName("active");
     for(let btn of activeButtons)
@@ -14,6 +23,7 @@ const loadCategory = () =>{
 }
 
 const loadCategoryVideos= (id) =>{
+    showLoader();
     const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
     fetch(url)
     .then((res) => res.json())
@@ -23,6 +33,35 @@ const loadCategoryVideos= (id) =>{
         clickedButton.classList.add('active');
         displayVideos(data.category)
     });
+}
+const loadVideoDetails = (videoId) =>{
+
+    console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) =>{
+        displayVideoDetails(data.video);
+    })
+}
+const displayVideoDetails =(video) =>{
+    console.log(video);
+    document.getElementById("video_details").showModal();
+    const detailsContainer = document.getElementById("details_container");
+    detailsContainer.innerHTML =`
+     <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</div>
+    `
 }
 
 function displayCategories(categories){
@@ -38,8 +77,9 @@ function displayCategories(categories){
 }
 // {category_id: '1001', video_id: 'aaaa', thumbnail: 'https://i.ibb.co/L1b6xSq/shape.jpg', title: 'Shape of You', authors: Array(1), …}
 // Load Videos
-const loadVideos = () =>{
-    fetch(" https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = (searchText = "") =>{
+    showLoader();
+    fetch( `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json())
     .then((data)=>{
         removeActiveClass();
@@ -48,6 +88,7 @@ const loadVideos = () =>{
     });
 }
 const displayVideos = (videos) =>{
+    hideLoader();
     const videoContainer = document.getElementById("video-container");
     videoContainer.innerHTML = ""; // Clear the container before adding new videos
     if(videos.length == 0)
@@ -80,17 +121,26 @@ const displayVideos = (videos) =>{
               </div>
               <div class="intro">
                 <h2 class="text-sm font-semibold">${video.title}</h2>
-                <p class="text-gray-500 text-sm  flex gap-2">${video.authors[0].profile_name}<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+                <p class="text-gray-500 text-sm  flex gap-2">
+                ${video.authors[0].profile_name}
+                ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">`:``}
+                
+                </p>
                 <p class="text-gray-500 text-sm">${video.others.views} views</p>
             </div>
             </div>
+            <button class="btn btn-block" onclick="loadVideoDetails('${video.video_id}')">Show Details</button>
         </div>
         `
         videoContainer.append(videoCard);
     })
+    
 }
 
-
+document.getElementById('search_input').addEventListener("keyup",(e)=>{
+    const input = e.target.value;
+    loadVideos(input);
+})
 
 
 loadCategory();
