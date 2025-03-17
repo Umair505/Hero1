c9119f2e116560e842a1c9f549cf2856544f8676
Umair505 <moinuli359@gmail.com>
2025-03-18 00:17:26 +0600

    localStorage

---
+++ MILESTONE/MILESTONE 6/Module 36.5/localStorage.js
@@ -0,0 +1,11 @@
+// const age = 10;
+// localStorage.setItem("userAge",age);
+// console.log(localStorage.getItem("userAge"));
+// const handleAddToStorage = () =>{
+//     const name = "Moinul Islam";
+//     localStorage.setItem("Name",name);
+// }
+const handleAddToStorage = () =>{
+    const name = "Moinul Islam";
+    localStorage.setItem("Name",name);
+}
