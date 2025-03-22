c68c6b6cd6c70aaf662078459adede2c48aeec9c
Umair505 <moinuli359@gmail.com>
2025-03-23 00:33:56 +0600

    Js recap module

---
+++ MILESTONE/MILESTONE 7/app.js
@@ -0,0 +1,26 @@
+// let name  = "Moinul";
+// name ="Moinul Islam Umair";
+
+// const countyName = "Bangladesh";
+// const a = 40;
+// if(a>20 || a<=30){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+// const friends = ["Hero","Srawler","Rahim","Karim"];
+// friends.push("BD");
+// console.log(friends);
+// function sum(a,b){
+//     const result = a+b;
+//     return result;
+// }
+// console.log(sum(1,2));
+const person = {
+    name : "Moinul",
+    friends : ["Daud","Dilon","Arafat"],
+    age : 24,
+    city : "Chittagong"
+}
+console.log(person.friends[0]);
