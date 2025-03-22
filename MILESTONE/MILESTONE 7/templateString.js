b8358e80bcc1117d627e7cf91d2224db6be1c774
Umair505 <moinuli359@gmail.com>
2025-03-23 00:40:36 +0600

    template string, arrow function, spread operator

---
+++ MILESTONE/MILESTONE 7/templateString.js
@@ -0,0 +1,13 @@
+// const fname = "Moinul";
+// const lname = "Islam";
+// console.log(`I am ${fname} ${lname}`);
+// Arrow Functions
+// const sum = (a,b) => a+b;
+
+// console.log(sum(5,10));
+
+// spread operator
+const numbers = [1,2,3,4,5,6,7,8];
+// console.log(...numbers);
+const newNumbers = [...numbers,9,10,11,12,13];
+console.log(...newNumbers);
