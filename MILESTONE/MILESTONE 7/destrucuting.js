c643683de861db0bf6077e5716d1c1e8c597021b
Umair505 <moinuli359@gmail.com>
2025-03-24 11:20:38 +0600

    Destructuring.js

---
+++ MILESTONE/MILESTONE 7/destrucuting.js
@@ -0,0 +1,15 @@
+// const friends = ["Daud","Dilon","Arafat"];
+
+// const [a,b,c] = friends
+
+// console.log(a,b,c); // Daud Dilon Arafat
+
+// const person ={
+//     name : "Moinul",
+//     age : 24,
+//     friends : ["Daud","Dilon","Arafat"],
+//     country : "Bangladesh"
+// }
+// const {name,age,friends,country} = person;
+// console.log(name);
+// console.log(friends);
