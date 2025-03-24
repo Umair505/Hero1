803b2bd94ec1e545880103d6d39dd59daafcba81
Umair505 <moinuli359@gmail.com>
2025-03-24 13:17:38 +0600

    explore react core

---
+++ MILESTONE/MILESTONE 7/Module 38/explore-rectcore/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Person></Person>
+      <Sports></Sports>
+    </>
+  )
+}
+function Person(){
+  const age = 23;
+  const name = "Moinul Islam"
+  return(
+    <div>
+      <h2>Hello, I am a person {name}.My Age is {age}</h2>
+    </div>
+  )
+}
+function Sports(){
+  return(
+    <div>
+      <h3>I love watching cricket</h3>
+    </div>
+  )
+}
+export default App
