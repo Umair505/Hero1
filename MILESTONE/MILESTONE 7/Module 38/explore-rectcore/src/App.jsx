8887aed49c77e5cd78e6b9c5c010110455305f3c
Umair505 <moinuli359@gmail.com>
2025-03-26 00:07:04 +0600

    updated

--- MILESTONE/MILESTONE 7/Module 38/explore-rectcore/src/App.jsx
+++ MILESTONE/MILESTONE 7/Module 38/explore-rectcore/src/App.jsx
@@ -17,24 +17,51 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Student></Student>
+      <Developer name="Moinul" tech="Python"></Developer>
+      <Developer name="Shabana" tech="Django"></Developer>
       <Person></Person>
-      <Sports></Sports>
     </>
   )
 }
 function Student(){
   return(
-    <div>
-      <h2>Hello, I am a student. My age is 15</h2>
+    <div className='student'>
+      <h2>Hello, I am a student. My age is 23</h2>
+      <p>Name : </p>
+      <p>Department : </p>
+    </div>
+  )
+}
+function Developer(props) {
+  console.log(props);
+  return (
+    <div style={
+      {
+        border: '1px solid',
+        borderRadius: '20px',
+      }
+    }>
+      <h2>Hello, I am {props.name} a developer. My experience is 5 years</h2>
+      <p>Technology {props.tech}</p>
     </div>
   )
 }
 function Person(){
   const age = 23;
   const name = "Moinul Islam"
+  const personStyle = {
+    color: 'red',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    border: '2px solid black',
+    padding: '10px',
+    borderRadius: '5px',
+    backgroundColor: 'white'
+  }
   return(
-    <div>
-      <h2>Hello, I am a person {name}.My Age is {age}</h2>
+    <div  id='' title='tooltip' style={personStyle}>
+      <h2 >Hello, I am a person {name}.My Age is {age}</h2>
     </div>
   )
 }
