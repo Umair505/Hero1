4a15ae6fb037282a4bd5e5a220155283cb6b6963
Umair505 <moinuli359@gmail.com>
2025-03-28 00:22:11 +0600

    Six ways to do Conditional Rendering: If, Ternary and AND OR

--- MILESTONE/MILESTONE 7/Module 38/explore-rectcore/src/todo.jsx
+++ MILESTONE/MILESTONE 7/Module 38/explore-rectcore/src/todo.jsx
@@ -3,15 +3,47 @@
 //         <li>Task :{task}</li>
 //     )
 // }
+// export default function ToDo({task,isDone}){
+//     if(isDone){
+//         return(
+//             <li>Done : {task}</li>
+//         )
+//     }
+//     else{
+//         return(
+//             <li>DO Now : {task}</li>
+//         )
+//     }
+// }
+
+// Conditional rendering: 3 ternary
+//conditional ? true : false
+
+// export default function ToDo({task,isDone,time = 0}){
+//     return isDone?<li>Done : {task} time : {time}</li>:
+//     <li>Not Done : {task}</li>
+
+// }
+//condional rendering : 4 &&
+// export default function ToDo({task,isDone,time = 0}){
+//     return isDone && <li>Done : {task} time:{time}</li>
+
+// }
+//condional rendering : 5 ||
+// export default function ToDo({task,isDone,time = 0}){
+//     return isDone || <li>Not Done task : {task} time:{time}</li>
+
+// }
+
 export default function ToDo({task,isDone}){
+    let listItem;
     if(isDone){
-        return(
-            <li>Done : {task}</li>
-        )
+            listItem = <li>Done : {task}</li>
     }
     else{
-        return(
-            <li>DO Now : {task}</li>
-        )
+       
+            listItem = <li>DO Now : {task}</li>
+        
     }
+    return listItem;
 }
