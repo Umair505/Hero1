853ad6a64030e3de1e9300a8bd144ba0ad9cdb82
Umair505 <moinuli359@gmail.com>
2025-03-28 20:18:42 +0600

    app.jsx

--- MILESTONE/MILESTONE 7/Module 39/react-core-concepts/src/App.jsx
+++ MILESTONE/MILESTONE 7/Module 39/react-core-concepts/src/App.jsx
@@ -2,12 +2,30 @@ import './App.css'
 
 function App() {
   
-
+  function handleClick(){
+    alert("I am Clicked")
+  }
+  const handleClick3 =() =>{
+    alert("I am Clicked 3");
+  }
+  const handleAdd5 = (num) =>{
+    const newNum = num + 5;
+    alert(newNum);
+  }
   return (
     <>
   
       <h1>Vite + React</h1>
+      <button onClick={handleClick}>Click Me</button>
+
+      <button onClick={function handleClick2(){
+        alert("I am Clicked 2");
+      }}>Click Me2</button>
+      <button onClick={handleClick3}>Click Me</button>
+      <button onClick={() => alert("Clicked 4")}>Click Me</button>
       
+      {/* <button onClick={handleAdd5(7)}>Click Add 5</button> */}
+      <button onClick={() => handleAdd5(100)}>Click Add 5</button>
     </>
   )
 }
