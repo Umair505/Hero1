8c3b3f22c479f044fdce01a621fed62e185123b7
Umair505 <moinuli359@gmail.com>
2025-03-29 13:05:24 +0600

    Use style in react and 3 column layout

--- MILESTONE/MILESTONE 7/Module 40/react-on-the-go/src/components/countries/Countries.jsx
+++ MILESTONE/MILESTONE 7/Module 40/react-on-the-go/src/components/countries/Countries.jsx
@@ -1,16 +1,18 @@
 //rsc
 import React, { use } from 'react';
 import Country from '../country/Country';
-
+import './Countries.css'
 const Countries = ({countriesPromise}) => {
     const countries = use(countriesPromise);
     console.log(countries)
     return (
         <div>
             <h1>Travelling Countries : {countries.length}</h1>
+            <div className='countries'>
             {
                 countries.map(country => <Country key={country.cca3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- MILESTONE/MILESTONE 7/Module 40/react-on-the-go/src/components/country/Country.jsx
+++ MILESTONE/MILESTONE 7/Module 40/react-on-the-go/src/components/country/Country.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import './Country.css'
 const Country = ({country}) => {
-    console.log(country);
+    const handleVisited = () =>{
+        alert('Visited');
+    }
     return (
-        <div>
+        <div className='country'>
             <h3>Name : {country.name.common}</h3>
             <img src={country.flags.png} alt="" />
             <p>Independent : {country?.independent ? 'Free': 'Not free'}</p>
             <p>Population : {country?.population}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
