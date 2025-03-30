4f9841c9647952e0775a160578c2e92918d60aef
Umair505 <moinuli359@gmail.com>
2025-03-31 00:13:35 +0600

    css added

--- MILESTONE/MILESTONE 7/Module 41/my-react/src/components/bottles/Bottles.jsx
+++ MILESTONE/MILESTONE 7/Module 41/my-react/src/components/bottles/Bottles.jsx
@@ -1,12 +1,12 @@
 import React, { use } from 'react';
 import Bottle from '../Bottle/Bottle';
-
+import './bottles.css'
 const Bottles = ({bottlesPromise}) => {
     const bottles = use(bottlesPromise);
     return (
         <div>
             <h3>Bottles : {bottles.length}</h3>
-            <div>
+            <div className='bottles-container'>
                 {
                     bottles.map(bottle => <Bottle bottle ={bottle} key={bottle.id}></Bottle>)
                 }
