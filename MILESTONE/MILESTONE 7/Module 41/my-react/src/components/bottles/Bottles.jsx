4be5073a40b60bcd357576811cf7f197db13bcab
Umair505 <moinuli359@gmail.com>
2025-04-01 09:17:34 +0600

    Add to cart added

--- MILESTONE/MILESTONE 7/Module 41/my-react/src/components/bottles/Bottles.jsx
+++ MILESTONE/MILESTONE 7/Module 41/my-react/src/components/bottles/Bottles.jsx
@@ -4,12 +4,14 @@ import './bottles.css'
 const Bottles = ({bottlesPromise}) => {
     const [cart,setCart] = useState([])
     const handleAddToCart =(bottle) =>{
-        console.log('add to cart',bottle.name);
+        const newCart = [...cart,bottle];
+        setCart(newCart);
     }
     const bottles = use(bottlesPromise);
     return (
         <div>
             <h3>Bottles : {bottles.length}</h3>
+            <p>Added to Cart: {cart.length}</p>
             <div className='bottles-container'>
                 {
                     bottles.map(bottle => <Bottle handleAddToCart={handleAddToCart} bottle ={bottle} key={bottle.id}></Bottle>)
