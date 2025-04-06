c58df4085e9fdabd0a2e1be1e4727e01f274c446
Umair505 <moinuli359@gmail.com>
2025-04-06 22:24:23 +0600

    Blogs.jsx added

--- MILESTONE/MILESTONE 7/Module 42/react-first-blogs/src/App.jsx
+++ MILESTONE/MILESTONE 7/Module 42/react-first-blogs/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Navbar></Navbar>
+     <Blogs></Blogs>
     </>
   )
 }
