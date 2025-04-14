316aa4c144c3f4abb969bd608e0549029925dfea
Umair505 <moinuli359@gmail.com>
2025-04-14 11:10:14 +0600

    dynamic menu button toggle and menu toggle

--- MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/Navbar/Navbar.jsx
+++ MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Menu, X } from 'lucide-react';
+import React, { useState } from 'react';
 const navLinks = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -6,19 +7,30 @@ const navLinks = [
     { id: 4, name: "Blog", path: "/blog" },
     { id: 5, name: "Contact", path: "/contact" }
   ];
+const links =  navLinks.map(link => (
+    <li key={link.id} className="flex mr-4">
+        <a href={link.path} className=" hover:text-blue-500">{link.name}</a>
+    </li>
+))
   
 const Navbar = () => {
+    const [open,setOpen] = useState(false);
     return (
-        <nav className='w-11/12 mx-auto'> 
-            <ul className='flex justify-between items-center bg-gray-800 text-white p-4'>
+        <nav className='flex justify-between mx-10 '> 
+            <span className='flex' onClick={()=>setOpen(!open)}>
+                { open ?<X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>}
+                
+            <ul className='md:hidden'>
+                {links}
+            </ul>
+                <h1 className='ml-4'>My Nav</h1>
+            </span>
+            <ul className='md:flex hidden mr-2'>
                 {
-                    navLinks.map(link => (
-                        <li key={link.id} className="inline-block mx-4">
-                            <a href={link.path} className=" hover:text-blue-500">{link.name}</a>
-                        </li>
-                    ))
+                   links
                 }
             </ul>
+            <ul><button className='btn'>Login</button></ul>
         </nav>
         // <div>
         //     <ul className='flex justify-between items-center bg-gray-800 text-white p-4'>
