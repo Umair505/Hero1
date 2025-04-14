e233ba609945e5353ad422dae9e057f42cb447f0
Umair505 <moinuli359@gmail.com>
2025-04-14 12:37:27 +0600

    Display Pricing options in Pricing Card

--- MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/App.jsx
+++ MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/App.jsx
@@ -1,15 +1,23 @@
+import { Suspense } from 'react'
 import './App.css'
 import DaisyNav from './components/DaisyNav/DaisyNav'
 import Navbar from './components/Navbar/Navbar'
+import PricingOptions from './components/PricingOptions/PricingOptions'
 
 function App() {
-
+  const pricingPromise =fetch('pricingData.json').then(res=>res.json())
+  
   return (
     <>
     <header>
     {/* <DaisyNav></DaisyNav> */}
     <Navbar></Navbar>
     </header>
+    <main>
+      <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
+        <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
+      </Suspense>
+    </main>
     </>
   )
 }
