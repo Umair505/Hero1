deb2415136a41e963e49b9eaeba84b3fb0c9cd01
Umair505 <moinuli359@gmail.com>
2025-04-16 23:53:46 +0600

    Explore Axios JS chart data processing and bar chart

--- MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/MarksChart/MarksChart.jsx
+++ MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/MarksChart/MarksChart.jsx
@@ -1,14 +1,31 @@
 import React, { use } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const MarksChart = ({marksPromise}) => {
     const marksDataRes = use(marksPromise);
     const marksData = marksDataRes.data;
-    console.log(marksData);
     //data processing for the chart
-    
+    const marksChartData = marksData.map(stdData => {
+        const student = {
+            id: stdData.id,
+            name: stdData.name,
+            physics: stdData.marks.phy,
+            chemistry: stdData.marks.Chemistry,
+            math: stdData.marks.math
+        }
+        const avg = (stdData.marks.phy + stdData.marks.Chemistry + stdData.marks.math) / 3;
+        student.avg = avg;
+        return student;
+    })
     return (
         <div>
-            
+            <BarChart width={500} height={300} data={marksChartData}>
+                <XAxis dataKey="name"></XAxis>
+                <YAxis></YAxis>
+                <Bar dataKey="avg" fill="yellow"></Bar>
+                <Bar dataKey="math" fill="blue"></Bar>
+                <Tooltip></Tooltip>
+            </BarChart>
         </div>
     );
 };
