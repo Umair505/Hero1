d91508e78cef19f6ba9a5b566eca9091ef509394
Umair505 <moinuli359@gmail.com>
2025-04-16 21:51:22 +0600

    updated

--- MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/PricingCard/PricingCard.jsx
+++ MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/PricingCard/PricingCard.jsx
@@ -5,19 +5,20 @@ const PricingCard = ({pricing}) => {
     console.log(pricing);
     const { id, name, price, description,features } = pricing;
     return (
-        <div className='border text-black bg-amber-200 p-4 rounded-2xl shadow-lg'>
+        <div className='flex flex-col border text-black bg-amber-200 p-4 rounded-2xl shadow-lg'>
             <div>
                 <h1 className='text-7xl'>{name}</h1>
                 <h4 className='text-3xl'>{price}</h4>
             </div>
             {/* card body  */}
-            <div>
+            <div className='flex-1 m-2'>
                 <p className='text-xl pt-3'>{description}</p>
                 {
                     features.map((feature,index) =><PricingFeature feature={feature} key={index}></PricingFeature>)           
 
                 }
             </div>
+            <button className='btn w-full mt-4'>Subscribe</button>
         </div>
         
 
