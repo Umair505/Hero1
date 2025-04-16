2cd59db1ae54f95611766f73f977643c06266a8e
Umair505 <moinuli359@gmail.com>
2025-04-16 22:04:37 +0600

    Use Daisy UI pricing card to modify for the component

--- MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/PricingOptions/PricingOptions.jsx
+++ MILESTONE/MILESTONE 8/MODULE 44/tailwind-react-pricing-options/src/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricing from '../DaisyPricingCard/DaisyPricing';
 
 const PricingOptions = ({pricingPromise}) => {
     const pricingData = use(pricingPromise);
@@ -7,8 +8,12 @@ const PricingOptions = ({pricingPromise}) => {
         <div>
             <h2 className='text-xl'>Get Our Membership</h2>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                {
+                {/* {
                     pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+                    } */}
+                {
+                    pricingData.map(pricing => <DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>)
+
                 }
             </div>
         </div>
