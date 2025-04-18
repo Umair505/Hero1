16773afba37f45e633a1868095704f7a5624bee9
Umair505 <moinuli359@gmail.com>
2025-04-18 10:33:30 +0600

    Multiple ways to load data using loader and useLoaderData

--- MILESTONE/MILESTONE 8/MODULE 45/intro-to-react-router/src/main.jsx
+++ MILESTONE/MILESTONE 8/MODULE 45/intro-to-react-router/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -10,7 +10,10 @@ import Home from './Components/Home/Home.jsx';
 import Root from './Components/Root/Root.jsx';
 import Mobiles from './Components/Mobiles/Mobiles.jsx';
 import Laptop from './Components/laptops/Laptop.jsx';
+import Users from './Components/Users/Users.jsx';
+import UsersTwo from './Components/Users/UsersTwo.jsx';
 
+const usersPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,7 +21,21 @@ const router = createBrowserRouter([
     children: [
       {index: true,Component: Home},
       {path:'mobiles',Component:Mobiles},
-      {path:'laptop',Component:Laptop}
+      {path:'laptop',Component:Laptop},
+      {
+        path:'users',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
+        Component:Users
+      },
+      {
+        path: 'users2',
+        element: 
+        <Suspense fallback={<span>Loading.....</span>}>
+          <UsersTwo usersPromise={usersPromise}></UsersTwo>
+        </Suspense>,
+        
+
+      }
     ]
   },
   {
