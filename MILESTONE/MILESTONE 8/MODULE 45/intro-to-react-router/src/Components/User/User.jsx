298c6cfa7112ee0bd68863e75cac929c2b901871
Umair505 <moinuli359@gmail.com>
2025-04-19 09:43:28 +0600

    Navigate useLocation and Module Summary

--- MILESTONE/MILESTONE 8/MODULE 45/intro-to-react-router/src/Components/User/User.jsx
+++ MILESTONE/MILESTONE 8/MODULE 45/intro-to-react-router/src/Components/User/User.jsx
@@ -1,10 +1,11 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useLocation } from 'react-router';
 import UserDetailsTwo from '../userDetails2/UserDetailsTwo';
 
 const User = ({user}) => {
     const [showInfo,setShowInfo] = useState(false)
     const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${user.id}`).then(res => res.json());
+    const location = useLocation();
     const {id,name,phone,email} = user;
     const userStyle ={
         border: '2px solid yellow',
@@ -12,6 +13,10 @@ const User = ({user}) => {
         padding: '10px',
         borderRadius: '10px',
         
+    }
+    const [visitHome,setVisitHome] = useState(false);
+    if(visitHome){
+        return <Navigate to="/"></Navigate>
     }
     return (
         <div style={userStyle}>
@@ -26,6 +31,7 @@ const User = ({user}) => {
                     <UserDetailsTwo userPromise={userPromise}></UserDetailsTwo>
                 </Suspense>
             }
+            <button onClick={() => setVisitHome(true)}>Visit Home</button>
         </div>
     );
 };
