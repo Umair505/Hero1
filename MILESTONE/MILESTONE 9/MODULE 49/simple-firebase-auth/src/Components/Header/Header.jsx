bdeb1ccb9f9be1d4fd2e738c395aefc0718d1fbb
Umair505 <moinuli359@gmail.com>
2025-04-29 18:14:58 +0600

    first commit

---
+++ MILESTONE/MILESTONE 9/MODULE 49/simple-firebase-auth/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const Header = () => {
+    return (
+        <nav>
+            <ul>
+                <NavLink to="/">
+                    Home
+                </NavLink>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
