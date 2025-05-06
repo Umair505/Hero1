0188721c6c2454f1c2d4fa164e8b9aade04b636c
Umair505 <moinuli359@gmail.com>
2025-05-06 09:38:14 +0600

    Load News Data & Filter News Data based on Category

--- MILESTONE/MILESTONE 9/MODULE 52/dragon-news-code/src/pages/CategoryNews.jsx
+++ MILESTONE/MILESTONE 9/MODULE 52/dragon-news-code/src/pages/CategoryNews.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} = useParams();
+    const data = useLoaderData();
 
+    const [categoryNews,setCategoryNews] = useState([]);
+    // console.log(id,data);
+    useEffect(()=>{
+        if(id=='0'){
+            setCategoryNews(data);
+            return;
+        }
+        else if(id == '1')
+        {
+            const filterNews = data.filter(news=>news.others.is_today_pick==true)
+            setCategoryNews(filterNews);
+            return;
+        }
+        else{
+            const filterNews = data.filter(news=>news.category_id == id)
+            setCategoryNews(filterNews);
+        }
+    },[data,id])
     return (
         <div>
             <h1>Category News - {id}</h1>
+            <h1> Total News {categoryNews.length}</h1>
         </div>
     );
 };
